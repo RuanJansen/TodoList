a8f889ebdc5d6990eadd342883c0ea4669c3b439
Ruan Jansen <ruan.d.jansen@gmail.com>
2022-08-23 09:55:05 +0200

    remove buttons from TaskView

--- TodoList/TaskView.swift
+++ TodoList/TaskView.swift
@@ -12,58 +12,29 @@ struct TaskView: View {
     var viewController = TodoListViewController()
     var body: some View {
         NavigationView {
-            ZStack {
-                VStack{
-                    HStack{
-                        Text("\(task.entryDate.formatted())")
-                        Spacer()
-                        Text("\(task.dueDate.formatted())")
-                    }.padding(.bottom)
-                    HStack {
-                        Text("Description")
-                        Spacer()
-                    }.padding(.bottom).font(.title2)
-                    
-                    ScrollView {
-                        Text("\(task.description)")
-                    }
-                    .frame(height: 200)
-                    Spacer()
-                }
-                VStack{
-                    Spacer()
-                    HStack{
-                        ZStack {
-                            Capsule()
-                                .frame(width: 130, height: 50)
-                                .foregroundColor(.green)
-                            Button("Mark as done") {
-                                viewController.archiveTask()
-                            }.foregroundColor(.white)
-                        }
-                        Spacer()
-                        ZStack {
-                            Capsule()
-                                .frame(width: 130, height: 50)
-                                .foregroundColor(.blue)
-                            Button("Archive") {
-                                viewController.archiveTask()
-                            }.foregroundColor(.white)
-                        }
-                    }.padding(.bottom)
-                }
-            }.navigationTitle("\(task.title)").padding()
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button{
-                            
-                        }label: {
-                            Label("Delete", systemImage: "trash.circle.fill")
-                        }
-                    }
-                }
             
-        }
+            VStack{
+                HStack{
+                    Text("\(task.entryDate.formatted())")
+                    Spacer()
+                    Text("\(task.dueDate.formatted())")
+                }.padding(.bottom)
+                HStack {
+                    Text("Description")
+                    Spacer()
+                }.padding(.bottom).font(.title2)
+                
+                ScrollView {
+                    Text("\(task.description)")
+                }
+                .frame(height: 200)
+                Spacer()
+            }
+            
+        }.navigationTitle("\(task.title)").padding()
+        
+        
+        
     }
 }
 
