667289d2e1ea55e65882a795b62ed31b42711c1e
RuanJansen <ruan.d.jansen@gmail.com>
2022-09-08 16:48:35 +0200

    Add all task view

--- TodoList/Views/TodoListView.swift
+++ TodoList/Views/TodoListView.swift
@@ -15,12 +15,47 @@ struct TodoListView: View {
     @State var isCompleted = false
     @State var isOverdue = false
     @State var isArchived = false
-    //    @State var isDate = true
+    @State var showWeek = true
     var body: some View {
         NavigationView {
             VStack {
-                CalendarComponent()
-                    .frame(height: 125)
+                HStack{
+                    Button{
+                        withAnimation{
+                            showWeek = true
+                        }
+                    }label:{
+                        ZStack{
+                            Capsule()
+                                .opacity(showWeek ? 1 : 0)
+                                .foregroundColor(.blue)
+                                .frame(width: 100, height: 25)
+                            Text("Week")
+                                .foregroundColor(showWeek ? .white : .blue)
+                        }
+                    }
+                    
+                    Button{
+                        withAnimation{
+                            showWeek = false
+                        }
+                    }label:{
+                        ZStack{
+                            Capsule()
+                                .opacity(!showWeek ? 1 : 0)
+                                .foregroundColor(.blue)
+                                .frame(width: 100, height: 25)
+                            Text("all")
+                                .foregroundColor(!showWeek ? .white : .blue)
+                        }
+                    }
+                    
+                    
+                }
+                if showWeek {
+                    CalendarComponent()
+                        .frame(height: 125)
+                }
                 
                 HStack{
                     Button{
@@ -49,24 +84,14 @@ struct TodoListView: View {
                         }
                     }
                     
-                    Spacer()
-                    Button{
-                        isArchived.toggle()
-                    }label:{
-                        ZStack{
-                            Capsule()
-                                .opacity(isArchived ? 1 : 0)
-                                .foregroundColor(.blue)
-                                .frame(width: 100, height: 25)
-                            Text("Archived")
-                                .foregroundColor(isArchived ? .white : .blue)
-                        }
-                    }
-                    
                 }.padding()
                 
                 List{
-                    TaskList(provider: provider, isCompleted: $isCompleted, isOverdue: $isOverdue, isArchived: $isArchived)
+                    if showWeek {
+                        WeekList(provider: provider, isCompleted: $isCompleted, isOverdue: $isOverdue)
+                    } else {
+                        TaskList(provider: provider, isCompleted: $isCompleted, isOverdue: $isOverdue)
+                    }
                 }.ignoresSafeArea()
                     .sheet(isPresented: $showAddTask){
                         AddTaskView()
@@ -75,11 +100,11 @@ struct TodoListView: View {
                         ToolbarItem(placement: .navigationBarLeading){
                             Text(Date().formatted(date: .abbreviated, time: .omitted))
                         }
-//                        ToolbarItem(placement: .navigationBarTrailing) {
-//                            NavigationLink(destination: ArchivedListView()){
-//                                Label("Archive", systemImage: "archivebox")
-//                            }
-//                        }
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            NavigationLink(destination: ArchivedListView()){
+                                Label("Archive", systemImage: "archivebox")
+                            }
+                        }
                         ToolbarItem {
                             Button{
                                 showAddTask = true
@@ -121,7 +146,6 @@ struct TaskList: View {
     var provider = Provider()
     @Binding var isCompleted: Bool
     @Binding var isOverdue: Bool
-    @Binding var isArchived: Bool
     
     @StateObject var calendarModel = CalendarViewModel()
     
@@ -136,11 +160,9 @@ struct TaskList: View {
     
     var body: some View {
         ForEach(tasks.filter {(
-            $0.isArchived == isArchived
+            !$0.isArchived
             && $0.isDone == isCompleted)
             && (calcIsOverdue(dueDate: $0.dueDate ?? Date()) == isOverdue)
-            && isSameDay(date1: $0.dueDate ?? Date(), date2: calendarModel.currentDay)
-//            && ($0.dueDate == calendarModel.currentDay)
         }){ task in
             var taskItem = task
             HStack {
@@ -159,9 +181,97 @@ struct TaskList: View {
                     
                     
                 }
-            }.onAppear{
-                print("LOL")
-                print(task.dueDate)
+            }
+            .swipeActions(edge: .trailing){
+                Button{
+                    archiveTask(task: task)
+                } label: {
+                    Label("Archive", systemImage: "archivebox")
+                    
+                }.tint(.blue)
+            }
+            .swipeActions(edge: .leading){
+                Button{
+                    doneTask(task: task)
+                } label: {
+                    Label("Done", systemImage: "checkmark")
+                }.tint(.blue)
+            }
+        }
+        .onMove(perform: { indices, newOffset in
+            provider.moveTask(indices: indices, newOffset: newOffset)
+        })
+    }
+    
+    func isSameDay(date1: Date, date2: Date) -> Bool {
+        let diff = Calendar.current.dateComponents([.day], from: date1, to: date2)
+        if diff.day == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func calcIsOverdue(dueDate: Date) -> Bool{
+        if dueDate > Date(){
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func doneTask(task: Task){
+        task.isDone.toggle()
+        try? moc.save()
+    }
+    
+    func archiveTask(task: Task){
+        task.isArchived = true
+        try? moc.save()
+    }
+    
+}
+
+struct WeekList: View {
+    var provider = Provider()
+    @Binding var isCompleted: Bool
+    @Binding var isOverdue: Bool
+    
+    @StateObject var calendarModel = CalendarViewModel()
+    
+    @FetchRequest(sortDescriptors: []) var tasks: FetchedResults<Task>
+    @Environment(\.managedObjectContext) var moc
+    
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "d MMM yyyy"
+        return formatter
+    }()
+    
+    var body: some View {
+        ForEach(tasks.filter {(
+            !$0.isArchived
+            && $0.isDone == isCompleted)
+            && (calcIsOverdue(dueDate: $0.dueDate ?? Date()) == isOverdue)
+            && isSameDay(date1: $0.dueDate ?? Date(), date2: calendarModel.currentDay)
+        }){ task in
+            var taskItem = task
+            HStack {
+                NavigationLink(destination: TaskView(taskItem: task, title: task.title ?? "Unknown Task", description: task.taskDescription ?? "No description", entryDate: task.entryDate ?? Date(), dueDate: task.dueDate ?? Date())){
+                    if task.isDone {
+                        Text("\(task.title ?? "Unknown")")
+                            .strikethrough()
+                        Spacer()
+                        Text("\(task.dueDate ?? Date(), formatter: TaskList.dateFormatter )")
+                            .strikethrough()
+                    } else {
+                        Text("\(task.title ?? "Unknown")")
+                        Spacer()
+                        Text("\(task.dueDate ?? Date(), formatter: TaskList.dateFormatter )")
+                    }
+                    
+                    
+                }
             }
             .swipeActions(edge: .trailing){
                 Button{
