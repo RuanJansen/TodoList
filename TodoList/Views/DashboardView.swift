65841170cc887478034b4a05633f602e4ca2cdd8
RuanJansen <ruan.d.jansen@gmail.com>
2022-09-14 14:35:20 +0200

    Edit dashboard

--- TodoList/Views/DashboardView.swift
+++ TodoList/Views/DashboardView.swift
@@ -27,65 +27,118 @@ struct DashboardView: View {
     @State var percentageArchived: Float = 0.0
     @State var progressbarColorArchived = Gradient(colors: [.white])
     @State var statTypeArchived: String = ""
-
+    
+    
+    @State var showWeek: Bool = false
+    @State var isArchive: Bool = false
+    @State var isCompleted: Bool = false
+    @State var isOverdue: Bool = false
+    @State var selectedCategory: String = ""
+    @State var categoryActive: Bool = false
     
     var body: some View {
         if tasks.isEmpty{
             Text("No data to show")
         }else{
             NavigationView {
-                ScrollView{
-                    VStack {
-                        ProgressBar(statType: $statTypeCompleted, percentage: $percentageCompleted, value: $valueCompleted, total: $totalCompleted, color: $progressbarColorCompleted)
-                            .onAppear(){
-                                let all = tasks.filter {!$0.isArchived}
-                                let total = all.count
-                                let task = all.filter{$0.isDone}.count
-                                let percentage = Float(task)/Float(total)
-                                self.valueCompleted = Float(task)
-                                self.totalCompleted = Float(total)
-                                self.percentageCompleted = percentage
-                                self.progressbarColorCompleted = Gradient(colors: [.green, .mint, .mint, .green])
-                                self.statTypeCompleted = "All Tasks"
+                Form {
+                    Section(header: Text("Charts")) {
+                        ScrollView(.horizontal) {
+                            HStack {
+        //                        NavigationLink(destination: {
+        //                            VStack {
+        //                                Form {
+        //                                    Section(header: Text(statTypeCompleted)) {
+        //
+        //                                        List{
+        //                                            ListComponent(showWeek: $showWeek, isArchive: $isArchive, isCompleted: $isCompleted, isOverdue: $isOverdue, selectedCategory: $selectedCategory, categoryActive: $categoryActive)
+        //                                        }
+        //                                    }
+        //                                }
+        //                            }
+        //                        })
+        //                        {
+        //                            VStack {
+        //                                ProgressBar(statType: $statTypeCompleted, percentage: $percentageCompleted, value: $valueCompleted, total: $totalCompleted, color: $progressbarColorCompleted)
+        //                                    .onAppear(){
+        //                                        let all = tasks.filter {!$0.isArchived}
+        //                                        let total = all.count
+        //                                        let task = all.filter{$0.isDone}.count
+        //                                        let percentage = Float(task)/Float(total)
+        //                                        self.valueCompleted = Float(task)
+        //                                        self.totalCompleted = Float(total)
+        //                                        self.percentageCompleted = percentage
+        //                                        self.progressbarColorCompleted = Gradient(colors: [.green, .mint, .mint, .green])
+        //                                        self.statTypeCompleted = "All Tasks"
+        //                                    }
+        //
+        //                            }
+        //                        }
+                                
+                                VStack {
+                                    ProgressBar(statType: $statTypeCompleted, percentage: $percentageCompleted, value: $valueCompleted, total: $totalCompleted, color: $progressbarColorCompleted)
+                                        .onAppear(){
+                                            let all = tasks.filter {!$0.isArchived}
+                                            let total = all.count
+                                            let task = all.filter{$0.isDone}.count
+                                            let percentage = Float(task)/Float(total)
+                                            self.valueCompleted = Float(task)
+                                            self.totalCompleted = Float(total)
+                                            self.percentageCompleted = percentage
+                                            self.progressbarColorCompleted = Gradient(colors: [.green, .mint, .mint, .green])
+                                            self.statTypeCompleted = "All Tasks"
+                                        }
+                                    
+                                }
+                                
+                                
+                                
+                                VStack {
+                                    ProgressBar(statType: $statTypeOverdue, percentage: $percentageOverdue, value: $valueOverdue, total: $totalOverdue, color: $progressbarColorOverdue)
+                                        .onAppear(){
+                                            let overdue = tasks.filter{$0.dueDate ?? Date() < Date() && !$0.isArchived}
+                                            let total = overdue.count
+                                            let task = overdue.filter{$0.isDone}.count
+                                            let percentage = Float(task)/Float(total)
+                                            self.valueOverdue = Float(task)
+                                            self.totalOverdue = Float(total)
+                                            self.percentageOverdue = percentage
+                                            self.progressbarColorOverdue = Gradient(colors: [.red, .orange, .orange, .red])
+                                            self.statTypeOverdue = "Overdue Tasks"
+                                        }
+                                    
+                                }
+                                
+                                VStack {
+                                    ProgressBar(statType: $statTypeArchived, percentage: $percentageArchived, value: $valueArchived, total: $totalArchived, color: $progressbarColorArchived)
+                                        .onAppear(){
+                                            let overdue = tasks.filter{$0.isArchived}
+                                            let total = overdue.count
+                                            let task = overdue.filter{$0.isDone}.count
+                                            let percentage = Float(task)/Float(total)
+                                            self.valueArchived = Float(task)
+                                            self.totalArchived = Float(total)
+                                            self.percentageArchived = percentage
+                                            self.progressbarColorArchived = Gradient(colors: [.blue, .cyan, .cyan, .blue])
+                                            self.statTypeArchived = "Archived Tasks"
+                                        }
+                                    
+                                }
+                                
+                                
                             }
-                        
+                            .navigationTitle("Completion Summary")
+                        }
                     }
                     
-                    VStack {
-                        ProgressBar(statType: $statTypeOverdue, percentage: $percentageOverdue, value: $valueOverdue, total: $totalOverdue, color: $progressbarColorOverdue)
-                            .onAppear(){
-                                let overdue = tasks.filter{$0.dueDate ?? Date() < Date() && !$0.isArchived}
-                                let total = overdue.count
-                                let task = overdue.filter{$0.isDone}.count
-                                let percentage = Float(task)/Float(total)
-                                self.valueOverdue = Float(task)
-                                self.totalOverdue = Float(total)
-                                self.percentageOverdue = percentage
-                                self.progressbarColorOverdue = Gradient(colors: [.red, .orange, .orange, .red])
-                                self.statTypeOverdue = "Overdue Tasks"
-                            }
-                        
+                    Section(header: Text("Categories")){
+//                        NavigationStack{
+//                            List{
+//                                NavigationLink("")
+//                            }
+//                        }
                     }
-                    
-                    VStack {
-                        ProgressBar(statType: $statTypeArchived, percentage: $percentageArchived, value: $valueArchived, total: $totalArchived, color: $progressbarColorArchived)
-                            .onAppear(){
-                                let overdue = tasks.filter{$0.isArchived}
-                                let total = overdue.count
-                                let task = overdue.filter{$0.isDone}.count
-                                let percentage = Float(task)/Float(total)
-                                self.valueArchived = Float(task)
-                                self.totalArchived = Float(total)
-                                self.percentageArchived = percentage
-                                self.progressbarColorArchived = Gradient(colors: [.blue, .cyan, .cyan, .blue])
-                                self.statTypeArchived = "Archived Tasks"
-                            }
-                        
-                    }
-                    
-                    
                 }
-                .navigationTitle("Completion Summary")
             }
         }
         
@@ -100,10 +153,10 @@ struct ProgressBar: View{
     @Binding var color: Gradient
     var body: some View{
         ZStack{
-            RoundedRectangle(cornerRadius: 30)
-                .opacity(0.1)
-                .foregroundColor(.white)
-                .frame(width: 350, height: 350, alignment: .center)
+//            RoundedRectangle(cornerRadius: 30)
+//                .opacity(0.1)
+//                .foregroundColor(.white)
+//                .frame(width: 350, height: 350, alignment: .center)
             Circle()
                 .stroke(lineWidth: 20.0)
                 .opacity(0.20)
@@ -132,7 +185,7 @@ struct ProgressBar: View{
                     .font(.title3)
             }
         }
-        .padding()
+        .padding(30)
     }
     
     func progressBarValue()->String{
